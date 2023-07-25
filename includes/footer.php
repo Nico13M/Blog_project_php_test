943f29f915a4312adf193743f6325aa76ed03890
Nico13M <martinez.nicolas13320@gmail.com>
2023-07-25 09:36:13 +0200

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer>
+
+        <p>Tous droits réservés © 2019 | 2023</p>
+        </footer>
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header class="logo">
+    <a href="/" class="logo">Nico Blog</a>
+    <ul class="header-menu">
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
+            <a href="/form-article.php">Écrire un article</a>
+        </li>
+    </ul>
+</header>
